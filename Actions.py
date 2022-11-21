694256bcce4497d556c808b6e31f27441fa6005e
papos8 <panagiotispapadamos@gmail.com>
2022-11-21 18:21:01 +0100

    Create file for actions

---
+++ Actions.py
@@ -0,0 +1,27 @@
+from msilib.schema import ActionText
+import Agent
+
+
+class Action:
+    def __init__(self) -> None:
+        pass
+
+
+class TurnLeft(Action):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class TurnRight(Action):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class Reverse(Action):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class MoveForward(Action):
+    def __init__(self) -> None:
+        super().__init__()
--- Agent.py
+++ Agent.py
@@ -1,5 +1,10 @@
-class Agent{
-    
+class Agent:
+    def __init__(self, SD, Resources):
+        self.SD = SD
+        self.Resources = Resources
 
 
-}
+class Player(Agent):
+    def __init__(self, SD, Position):
+        super().__init__(SD)
+        self.Position = Position
