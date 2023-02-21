3600fa71812f906b7806729d6e1bab3b2839e9fc
papos8 <panagiotispapadamos@gmail.com>
2023-02-21 17:28:50 +0100

    Time Testing

--- main.py
+++ main.py
@@ -1609,13 +1609,20 @@ file.close()
 # Randomly created test for Anchoring Bias Conditioning
 file = open("Randomly_Created_Tests/AnchoringBiasConditioning.txt", "w")
 file.write(
-    "Compare unbiased conditioning and anchoring bias conditioning.\n")
+    "Compare unbiased conditioning and anchoring bias conditioning.\nIn these tests we also compare when the two methods idenitfy the actual world to see if anchoring bias conditioning is faster than unbiased conditionin.\n")
 file.write("""First states and observables are created and then the plausibility orders of the agents.\nThe two agents have the same plausibility orders, but different stubbornness degrees. \n\n""")
 file.write(str(numberOfTests) +
            " randomly created tests will be generated and the success percentage will be displayed in the end.\n")
 unbiasedSuccess = 0
 biasedSuccess = 0
+counterForTimeEqual = 0
+counterForTimeUnbiased = 0
+counterForTimeBiased = 0
 for i in range(numberOfTests):
+    unbiasedCounter = 0
+    biasedCounter = 0
+    unbiasedBoolean = False
+    biasedBoolean = False
     states = States.States(5)
     newStates = copy.deepcopy(states)
     obs = Observables(states)
@@ -1670,12 +1677,17 @@ for i in range(numberOfTests):
             file.write(
                 key + ":" + str(unbiasedAgent.plausibilityOrder.getWorldsRelation()[key]) + ", ")
         file.write("\n")
+        if len(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1 and list(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] == states.getActualWorld():
+            unbiasedCounter += 1
+        else:
+            unbiasedCounter = 0
     if len(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1:
         file.write("Unbiased agent's most plausible world: " +
                    list(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] + "\n")
         if len(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1 and list(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] == states.getActualWorld():
             file.write("Unbiased agent identified the actual world!\n\n")
             unbiasedSuccess += 1
+            unbiasedBoolean = True
         else:
             file.write(
                 "Unbiased agent failed to identify the actual world!\n\n")
@@ -1703,6 +1715,11 @@ for i in range(numberOfTests):
             file.write(
                 key + ":" + str(biasedAgent.plausibilityOrder.getWorldsRelation()[key]) + ", ")
         file.write("\n")
+        if len(biasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1 and list(biasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] == states.getActualWorld():
+            biasedCounter += 1
+            biasedBoolean = True
+        else:
+            biasedCounter = 0
     file.write("\n")
     if len(biasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1:
         file.write("Biased agent's most plausible world: " +
@@ -1716,15 +1733,28 @@ for i in range(numberOfTests):
         file.write("Biased agent's most plausible worlds: " + ('-').join(str(world)
                                                                          for world in biasedAgent.plausibilityOrder.getMostPlausibleWorlds()) + "\n")
         file.write("Biased agent failed to identifiy the actual world!\n\n")
-print(unbiasedSuccess)
-print(biasedSuccess)
+
+    if unbiasedBoolean and biasedBoolean:
+        if biasedCounter > 0 and unbiasedCounter > 0 and biasedCounter < unbiasedCounter:
+            counterForTimeBiased += 1
+        elif biasedCounter > 0 and unbiasedCounter > 0 and biasedCounter > unbiasedCounter:
+            counterForTimeUnbiased += 1
+        elif biasedCounter > 0 and unbiasedCounter > 0 and biasedCounter == unbiasedCounter:
+            counterForTimeEqual += 1
 file.write("Unbiased agent identified the actual world " +
            str((float(unbiasedSuccess/numberOfTests)*100)) + "% of the cases.\n")
 file.write("Biased agent identified the actual world " +
            str(float(round((biasedSuccess/numberOfTests)*100, 2))) + "% of the cases.\n")
 file.write("Biased agent identified " +
            str(float(round((biasedSuccess/unbiasedSuccess)*100, 2))) +
-           "% of the identifiable cases (actual worlds).\n")
+           "% of the identifiable cases (actual worlds).\n\n")
+
+file.write(str(float(round(counterForTimeBiased/(counterForTimeBiased+counterForTimeEqual+counterForTimeUnbiased)*100, 2))) +
+           "% of the identified worlds, biased agent identified the world faster than the unbiased.\n")
+file.write(str(float(round(counterForTimeUnbiased/(counterForTimeBiased+counterForTimeEqual+counterForTimeUnbiased)*100, 2))) +
+           "% of the identified worlds, unbiased agent identified the world faster than the biased.\n")
+file.write(str(float(round(counterForTimeEqual/(counterForTimeBiased+counterForTimeEqual+counterForTimeUnbiased)*100, 2))) +
+           "% of the identified worlds, the agents identified the actual world the same time.\n")
 file.close()
 '''
 
@@ -1732,13 +1762,20 @@ file.close()
 # Randomly created test for Anchoring Bias Lexicographic Revision
 file = open("Randomly_Created_Tests/AnchoringBiasLexRevision.txt", "w")
 file.write(
-    "Compare unbiased lexicographic and anchoring bias lexicographic revision.\nWe include these tests to investigate the affect of random choice in truth tracking, in case an agent needs to make a decision.\n")
+    "Compare unbiased lexicographic and anchoring bias lexicographic revision.\nIn these tests we also compare when the two methods idenitfy the actual world to see if anchoring bias lexicographic revision is faster than unbiased lexicographci revision.\n")
 file.write("""First states and observables are created and then the plausibility orders of the agents.\nThe two agents have the same plausibility orders, but different stubbornness degrees. \n\n""")
 file.write(str(numberOfTests) +
            " randomly created tests will be generated and the success percentage will be displayed in the end.\n")
 unbiasedSuccess = 0
 biasedSuccess = 0
+counterForTimeEqual = 0
+counterForTimeUnbiased = 0
+counterForTimeBiased = 0
 for i in range(numberOfTests):
+    unbiasedCounter = 0
+    biasedCounter = 0
+    unbiasedBoolean = False
+    biasedBoolean = False
     states = States.States(5)
     newStates = copy.deepcopy(states)
     obs = Observables(states)
@@ -1793,12 +1830,17 @@ for i in range(numberOfTests):
             file.write(
                 key + ":" + str(unbiasedAgent.plausibilityOrder.getWorldsRelation()[key]) + ", ")
         file.write("\n")
+        if len(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1 and list(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] == states.getActualWorld():
+            unbiasedCounter += 1
+        else:
+            unbiasedCounter = 0
     if len(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1:
         file.write("Unbiased agent's most plausible world: " +
                    list(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] + "\n")
         if len(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1 and list(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] == states.getActualWorld():
             file.write("Unbiased agent identified the actual world!\n\n")
             unbiasedSuccess += 1
+            unbiasedBoolean = True
         else:
             file.write(
                 "Unbiased agent failed to identify the actual world!\n\n")
@@ -1826,6 +1868,10 @@ for i in range(numberOfTests):
             file.write(
                 key + ":" + str(biasedAgent.plausibilityOrder.getWorldsRelation()[key]) + ", ")
         file.write("\n")
+        if len(biasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1 and list(biasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] == states.getActualWorld():
+            biasedCounter += 1
+        else:
+            biasedCounter = 0
     file.write("\n")
     if len(biasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1:
         file.write("Biased agent's most plausible world: " +
@@ -1833,35 +1879,55 @@ for i in range(numberOfTests):
         if len(biasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1 and list(biasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] == states.getActualWorld():
             file.write("Biased agent identified the actual world!\n\n")
             biasedSuccess += 1
+            biasedBoolean = True
         else:
             file.write("Biased agent failed to identifiy the actual world!\n\n")
     else:
         file.write("Biased agent's most plausible worlds: " + ('-').join(str(world)
                                                                          for world in biasedAgent.plausibilityOrder.getMostPlausibleWorlds()) + "\n")
         file.write("Biased agent failed to identifiy the actual world!\n\n")
-print(unbiasedSuccess)
-print(biasedSuccess)
+
+    if unbiasedBoolean and biasedBoolean:
+        if biasedCounter > 0 and unbiasedCounter > 0 and biasedCounter < unbiasedCounter:
+            counterForTimeBiased += 1
+        elif biasedCounter > 0 and unbiasedCounter > 0 and biasedCounter > unbiasedCounter:
+            counterForTimeUnbiased += 1
+        elif biasedCounter > 0 and unbiasedCounter > 0 and biasedCounter == unbiasedCounter:
+            counterForTimeEqual += 1
 file.write("Unbiased agent identified the actual world " +
            str((float(unbiasedSuccess/numberOfTests)*100)) + "% of the cases.\n")
 file.write("Biased agent identified the actual world " +
            str(float(round((biasedSuccess/numberOfTests)*100, 2))) + "% of the cases.\n")
 file.write("Biased agent identified " +
            str(float(round((biasedSuccess/unbiasedSuccess)*100, 2))) +
-           "% of the identifiable cases (actual worlds).\n")
+           "% of the identifiable cases (actual worlds).\n\n")
+
+file.write(str(float(round(counterForTimeBiased/(counterForTimeBiased+counterForTimeEqual+counterForTimeUnbiased)*100, 2))) +
+           "% of the identified worlds, biased agent identified the world faster than the unbiased.\n")
+file.write(str(float(round(counterForTimeUnbiased/(counterForTimeBiased+counterForTimeEqual+counterForTimeUnbiased)*100, 2))) +
+           "% of the identified worlds, unbiased agent identified the world faster than the biased.\n")
+file.write(str(float(round(counterForTimeEqual/(counterForTimeBiased+counterForTimeEqual+counterForTimeUnbiased)*100, 2))) +
+           "% of the identified worlds, the agents identified the actual world the same time.\n")
 file.close()
 '''
-
 '''
 # Randomly created test for Anchoring Bias Minimal Revision
 file = open("Randomly_Created_Tests/AnchoringBiasMinRevision.txt", "w")
 file.write(
-    "Compare unbiased minimal and anchoring bias minimal revision.\nWe include these tests to investigate the affect of random choice in truth tracking, in case an agent needs to make a decision.\n")
+    "Compare unbiased minimal and anchoring bias minimal revision.\nIn these tests we also compare when the two methods idenitfy the actual world to see if anchoring bias minimal revision is faster than unbiased minimal revision.\n")
 file.write("""First states and observables are created and then the plausibility orders of the agents.\nThe two agents have the same plausibility orders, but different stubbornness degrees. \n\n""")
 file.write(str(numberOfTests) +
            " randomly created tests will be generated and the success percentage will be displayed in the end.\n")
 unbiasedSuccess = 0
 biasedSuccess = 0
+counterForTimeEqual = 0
+counterForTimeUnbiased = 0
+counterForTimeBiased = 0
 for i in range(numberOfTests):
+    unbiasedCounter = 0
+    biasedCounter = 0
+    biasedBoolean = False
+    unbiasedBoolean = False
     states = States.States(5)
     newStates = copy.deepcopy(states)
     obs = Observables(states)
@@ -1916,12 +1982,17 @@ for i in range(numberOfTests):
             file.write(
                 key + ":" + str(unbiasedAgent.plausibilityOrder.getWorldsRelation()[key]) + ", ")
         file.write("\n")
+        if len(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1 and list(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] == states.getActualWorld():
+            unbiasedCounter += 1
+        else:
+            unbiasedCounter = 0
     if len(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1:
         file.write("Unbiased agent's most plausible world: " +
                    list(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] + "\n")
         if len(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1 and list(unbiasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] == states.getActualWorld():
             file.write("Unbiased agent identified the actual world!\n\n")
             unbiasedSuccess += 1
+            unbiasedBoolean = True
         else:
             file.write(
                 "Unbiased agent failed to identify the actual world!\n\n")
@@ -1949,6 +2020,10 @@ for i in range(numberOfTests):
             file.write(
                 key + ":" + str(biasedAgent.plausibilityOrder.getWorldsRelation()[key]) + ", ")
         file.write("\n")
+        if len(biasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1 and list(biasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] == states.getActualWorld():
+            biasedCounter += 1
+        else:
+            biasedCounter = 0
     file.write("\n")
     if len(biasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1:
         file.write("Biased agent's most plausible world: " +
@@ -1956,21 +2031,37 @@ for i in range(numberOfTests):
         if len(biasedAgent.plausibilityOrder.getMostPlausibleWorlds()) == 1 and list(biasedAgent.plausibilityOrder.getMostPlausibleWorlds())[0] == states.getActualWorld():
             file.write("Biased agent identified the actual world!\n\n")
             biasedSuccess += 1
+            biasedBoolean = True
         else:
             file.write("Biased agent failed to identifiy the actual world!\n\n")
     else:
         file.write("Biased agent's most plausible worlds: " + ('-').join(str(world)
                                                                          for world in biasedAgent.plausibilityOrder.getMostPlausibleWorlds()) + "\n")
         file.write("Biased agent failed to identifiy the actual world!\n\n")
-print(unbiasedSuccess)
-print(biasedSuccess)
+
+    if unbiasedBoolean and biasedBoolean:
+        if biasedCounter > 0 and unbiasedCounter > 0 and biasedCounter < unbiasedCounter:
+            counterForTimeBiased += 1
+        elif biasedCounter > 0 and unbiasedCounter > 0 and biasedCounter > unbiasedCounter:
+            counterForTimeUnbiased += 1
+        elif biasedCounter > 0 and unbiasedCounter > 0 and biasedCounter == unbiasedCounter:
+            counterForTimeEqual += 1
 file.write("Unbiased agent identified the actual world " +
            str((float(unbiasedSuccess/numberOfTests)*100)) + "% of the cases.\n")
 file.write("Biased agent identified the actual world " +
            str(float(round((biasedSuccess/numberOfTests)*100, 2))) + "% of the cases.\n")
+file.write("Biased agent identified " +
+           str(float(round((biasedSuccess/unbiasedSuccess)*100, 2))) +
+           "% of the identifiable cases (actual worlds).\n\n")
+
+file.write(str(float(round(counterForTimeBiased/(counterForTimeBiased+counterForTimeEqual+counterForTimeUnbiased)*100, 2))) +
+           "% of the identified worlds, biased agent identified the world faster than the unbiased.\n")
+file.write(str(float(round(counterForTimeUnbiased/(counterForTimeBiased+counterForTimeEqual+counterForTimeUnbiased)*100, 2))) +
+           "% of the identified worlds, unbiased agent identified the world faster than the biased.\n")
+file.write(str(float(round(counterForTimeEqual/(counterForTimeBiased+counterForTimeEqual+counterForTimeUnbiased)*100, 2))) +
+           "% of the identified worlds, the agents identified the actual world the same time.\n")
 file.close()
 '''
-
 '''
 # Randomly created test for In-Group Favoritism Conditioning
 file = open("Randomly_Created_Tests/InGroupFavoritismConditioning.txt", "w")
